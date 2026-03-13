7bfe72819954c6536040c448763532eb0a6958ea
Jash Merchant <merchantjash@gmail.com>
2026-03-12 22:28:21 -0700

    Attempt quiz on if-else and range-check

---
+++ KotlinBasics/app/src/main/java/com/jashmerchant/kotlinbasics/AgeControl.kt
@@ -0,0 +1,14 @@
+package com.jashmerchant.kotlinbasics
+
+fun main() {
+    print("Enter your age as a whole number: ")
+    var age = readln().toInt()
+
+    if (age>=40) {
+        println("You cannot go into the club, please go home.")
+    } else if (age in 18..39) {
+        println("You can enter the club.")
+    } else {
+        println("Age not verified, please contact support.")
+    }
+}
